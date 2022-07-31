3951aaef062af99208a7967b41f20250ca1d02f2
imlzo <56844000+imlzo@users.noreply.github.com>
2022-07-31 14:15:25 -0700

    Add postDate filter. Use in index.njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
   // Passthrough copy
   eleventyConfig.addPassthroughCopy("src/css");
+
+  // postDate filter - Date for posts
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+  });
 
   return {
     dir: {
